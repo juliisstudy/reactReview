4fd30369930b0dde1e06feb8ef553a6760924216
Juli <31259481+juliisstudy@users.noreply.github.com>
2023-09-04 17:36:36 -0400

    component example

---
+++ my-react-app/src/Car.js
@@ -0,0 +1,4 @@
+function CarFounction() {
+  return <h2>"I am a car"</h2>
+}
+export default CarFounction;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CarFounction from './Car.js';
 
-
-const helloElement = <h1>Hello react!</h1>
+// const helloElement = <h1>Hello react!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const x = 5;
+let text = "Goodbye";
+if(x < 10 ){
+  text = "Hello";
+}
 
-root.render(helloElement);
+// const myElement = <h1>{text}</h1>
+const myElement = <h1>{(x) < 10 ? "hello":"Goodbye"}</h1>
+
+// react components: class component and founction component
+class Car extends React.Component {
+  render() {
+    return <h2>Hi,I am a Car!</h2>
+  }
+}
+
+function Garage() {
+  return (
+    <>
+      <h1>what is in the Garage?</h1>
+      <CarFounction/>
+    </>
+  )
+}
+root.render(<Garage/>);
